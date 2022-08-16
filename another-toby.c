39069ad8f681cec039f9aece0a558d5033863176
Steve bob <mesivte1@gmail.com>
2022-08-16 19:48:20 -0400

    new adnacement

--- another-toby.c
+++ another-toby.c
@@ -5,5 +5,6 @@ int main()
     printf("Welcome.\n");
     int a = 90, b = 80, c;
     c = a * b;
-    printf("%d\n", c);
+    printf("The times of %d and %d is = %d\n", a, b ,c);
+    printf("Now to understand git branches with squash feature clip and clear.\n");
 }
